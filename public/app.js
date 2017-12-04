90111242e3ced35bd7843f8fe2840e1928a49363
Scott Murray <rai.five@gmail.com>
2017-12-04 16:12:17 +0000

    Space Cats Added

--- public/app.js
+++ public/app.js
@@ -1,3 +1,35 @@
-var addcat = function(){
-   
-}
+var addCat = function(name, favFood, imgURL){
+   // this.name = name;
+   // this.favFood = favFood;
+   // this.imgURL = imgURL;
+   var ul = document.createElement('ul');
+   ul.classList.add('cat');
+   var nameLi = document.createElement('li');
+   nameLi.innerText = "Name: " + name;
+   var faveFoodLi = document.createElement('li');
+   nameLi.innerText = "Favourite Food: " + favFood;
+   var imgLi = document.createElement('li');
+   imgLi.innerHTML = '<img src="' + imgURL + '"/>'
+   var img = document.createElement('img');
+   ul.appendChild(nameLi);
+   ul.appendChild(faveFoodLi);
+   ul.appendChild(imgLi);
+   // imgLi.appendChild(img);
+   var cats = document.getElementById('cats');
+   cats.appendChild(ul);
+};
+
+
+var catApp = function(){
+   addCat("Morty", "Space Rats", 'https://i.imgur.com/rBZ2QrQ.jpg');
+   addCat("Morty", "Space Rats", 'https://i.imgur.com/rBZ2QrQ.jpg');
+   addCat("Morty", "Space Rats", 'https://i.imgur.com/rBZ2QrQ.jpg');
+   addCat("Morty", "Space Rats", 'https://i.imgur.com/rBZ2QrQ.jpg');
+   addCat("Morty", "Space Rats", 'https://i.imgur.com/rBZ2QrQ.jpg');
+   addCat("Morty", "Space Rats", 'https://i.imgur.com/rBZ2QrQ.jpg');
+   addCat("Morty", "Space Rats", 'https://i.imgur.com/rBZ2QrQ.jpg');
+   addCat("Morty", "Space Rats", 'https://i.imgur.com/rBZ2QrQ.jpg');
+   addCat("Morty", "Space Rats", 'https://i.imgur.com/rBZ2QrQ.jpg');
+};
+
+window.onload = catApp;
